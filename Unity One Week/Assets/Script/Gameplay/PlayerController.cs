de82c56a18f299070498cdece122fad6a99b69b4
Ripandy Adha <ripandy.adha@gmail.com>
2019-07-06 12:20:58 +0900

    feat : basic player control

---
+++ Unity One Week/Assets/Script/Gameplay/PlayerController.cs
@@ -0,0 +1,9 @@
+﻿/* PlayerController.cs
+ * Author		: Ripandy Adha (ripandy.adha@kadinche.com, ripandy.adha@gmail.com)
+ **/
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+}
---
+++ Unity One Week/Assets/Script/Scenes/SceneGameplayController.cs
@@ -0,0 +1,27 @@
+﻿/* SceneGameplayController.cs
+ * Author		: Ripandy Adha (ripandy.adha@kadinche.com, ripandy.adha@gmail.com)
+ **/
+ 
+ using UnityEngine;
+ using DG.Tweening;
+
+public class SceneGameplayController : SceneControllerBase
+{
+    [SerializeField] PlayerController[] players;
+
+    int activePlayer = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            Vector2 cursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // RaycastHit2D[] hits = Physics2D.RaycastAll(cursor, Vector2.zero, 0f);
+            players[activePlayer].transform.DOMove(cursor, 1f);
+        }
+    }
+}
