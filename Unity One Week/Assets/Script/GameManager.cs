a918ed42094c412b13e19595149fd11ef4df31e2
Ripandy Adha <ripandy.adha@gmail.com>
2019-07-06 12:19:30 +0900

    feat : basic structure

---
+++ Unity One Week/Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameManager", menuName = "GameManager", order = 0)]
+public class GameManager : ScriptableObject
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!instance)
+                instance = Resources.Load<GameManager>(typeof(GameManager).ToString());
+            if (!instance)
+            {
+                var gm = Resources.FindObjectsOfTypeAll<GameManager>();
+                if (gm != null && gm.Length > 0)
+                    instance = Resources.FindObjectsOfTypeAll<GameManager>()[0];
+            }
+            if (!instance)
+                instance = CreateInstance<GameManager>();
+            return instance;
+        }
+    }
+}
---
+++ Unity One Week/Assets/Script/Scenes/SceneControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SceneControllerBase : MonoBehaviour
+{
+    [SerializeField] protected GameManager m_GameManager;
+
+    protected virtual void Awake()
+    {
+        if (m_GameManager == null)
+            m_GameManager = GameManager.Instance;
+    }
+
+    protected virtual void Start()
+    {
+        SetReactiveObservers();
+    }
+
+    protected virtual void SetReactiveObservers()
+    {
+    }
+}
