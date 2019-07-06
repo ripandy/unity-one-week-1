01babc14d29b2dda9dae4026b26e4b5d80ae4c6d
Ripandy Adha <ripandy.adha@gmail.com>
2019-07-06 12:41:24 +0900

    add : energy ball

---
+++ Unity One Week/Assets/Script/Gameplay/EnergyBall.cs
@@ -0,0 +1,25 @@
+﻿/* EnergyBall.cs
+ * Author		: Ripandy Adha (ripandy.adha@kadinche.com, ripandy.adha@gmail.com)
+ **/
+
+using UnityEngine;
+
+public class EnergyBall : MonoBehaviour
+{
+    public float Energy { get; private set; }
+
+    SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        Energy = 0f;
+    }
+
+    public void AddEnergy(int energy)
+    {
+        Energy += energy;
+        float s = 1f + Energy / 2;
+        transform.localScale = new Vector3(s, s, 1f);
+    }
+}
